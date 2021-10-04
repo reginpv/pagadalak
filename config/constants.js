28f42bd7c118990cd17b28390fd69ccde48e2007
reginpv <reginpv@gmail.com>
2021-10-04 20:52:50 +0800

    update login

--- config/constants.js
+++ config/constants.js
@@ -1,4 +1,4 @@
-export const HOSTNAME = process.env.HOSTNAME
+export const HOSTNAME = process.env.NEXT_PUBLIC_HOSTNAME
 
 export const SITE_TITLE = "Camiling Institution Finder"
 export const SITE_DESCRIPTION = "Welcome to Camiling institution finder Pagadalak"
