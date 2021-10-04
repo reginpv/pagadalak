a75f4f235dd906b0e2e93c373b2b036b9331c56f
reginpv <reginpv@gmail.com>
2021-10-04 19:03:33 +0800

    added

--- components/formSchoolEdit.js
+++ components/formSchoolEdit.js
@@ -73,6 +73,84 @@ export default function FormSchoolEdit({ className, schoolId,  school }){
         <span className="text-red-500 text-14px hidden px-1">Geo address is required</span>
       </div>
 
+      <div className="">
+        <h3 className="font-bold">Please select levels</h3>
+        {
+          [
+            {
+              name: "elementary",
+              label: "Elementary"
+            },
+            {
+              name: "juniorHighSchool",
+              label: "Junior high-school"
+            },
+            {
+              name: "seniorHighSchool",
+              label: "Senior high-school"
+            }
+          ].map((item,i)=>(
+            <div key={i} className="py-2 flex items-center justify-between">
+              <div className="whitespace-nowrap">
+                <input 
+                  id={item.name} 
+                  type="checkbox" 
+                  checked={stateSchool[`${item.name}`]} 
+                  name={item.name} 
+                  onChange={e=>setStateSchool({...stateSchool, [`${item.name}`]: stateSchool[`${item.name}`] ? false : true})
+                  }
+                />
+                <label htmlFor={item.name} className="capitalize"> <span className="truncate">{item.label}</span></label>
+              </div>
+              <div>
+                <select name={`${item.name}Tuition`} value={stateSchool[`${item.name}Tuition`]} className="border py-1 px-3" onChange={e=>setStateSchool({...stateSchool, [`${item.name}Tuition`]: e.target.value})}>
+                  <option value="">Please tuition range</option>
+                  <option value="Free">Free</option>
+                  <option value="P1-P20,000">P1-P20,000</option>
+                  <option value="P21,000-P39,999">P21,000-P39,999</option>
+                  <option value="P40,000-P59,999">P40,000-P59,999</option>
+                  <option value="P60,000 above">P60,000 above</option>
+                </select>
+              </div>
+            </div>
+          ))
+        }
+        
+      </div>
+
+      <div className="">
+        <h3 className="font-bold">Please select strategy</h3>
+        {
+          [
+            {
+              name: "modularLearning",
+              label: "Modular Learning"
+            },
+            {
+              name: "blendedLearning",
+              label: "Blended Learning"
+            },
+            {
+              name: "onlineLearning",
+              label: "Online Learning"
+            }
+          ].map((item,i)=>(
+            <div key={i} className="py-2">
+              <input 
+                id={item.name} 
+                type="checkbox" 
+                name={item.name} 
+                checked={stateSchool[`${item.name}`]}
+                onChange={e=>setStateSchool({...stateSchool, [`${item.name}`]: stateSchool[`${item.name}`] ? false : true})
+                  }
+              />
+              <label htmlFor={item.name} className="capitalize"> {item.label}</label>
+            </div>
+          ))
+        }
+        
+      </div>
+
       <div data-form-message></div>
 
       <button role="submit" className="border p-3 bg-gray-100">
